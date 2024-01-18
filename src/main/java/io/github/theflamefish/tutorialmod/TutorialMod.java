4b52fa8dc05c12ec1197731bc485c2ab0a130309
TheFlameFish <114826427+TheFlameFish@users.noreply.github.com>
2024-01-18 15:11:40 -0800

    Added Sapphire item

--- src/main/java/io/github/theflamefish/tutorialmod/TutorialMod.java
+++ src/main/java/io/github/theflamefish/tutorialmod/TutorialMod.java
@@ -1,6 +1,9 @@
 package io.github.theflamefish.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import io.github.theflamefish.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -26,6 +29,8 @@ public class TutorialMod {
     public TutorialMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         // Register ourselves for server and other game events we are interested in
@@ -44,7 +49,9 @@ public class TutorialMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
