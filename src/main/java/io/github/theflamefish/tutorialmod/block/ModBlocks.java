59d85a0c3e5c60b02c7434d300ea8b122f212d61
TheFlameFish <114826427+TheFlameFish@users.noreply.github.com>
2024-02-03 21:29:24 -0800

    Added drops for sapphire blocks and added sapphire ores

--- src/main/java/io/github/theflamefish/tutorialmod/block/ModBlocks.java
+++ src/main/java/io/github/theflamefish/tutorialmod/block/ModBlocks.java
@@ -3,10 +3,12 @@ package io.github.theflamefish.tutorialmod.block;
 import com.mojang.blaze3d.platform.Lighting;
 import io.github.theflamefish.tutorialmod.TutorialMod;
 import io.github.theflamefish.tutorialmod.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
@@ -22,20 +24,32 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
 
 
-
     public static final RegistryObject<Block> SAPPHIRE_BLOCK = registerBlock("sapphire_block",
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK)
                     .sound(SoundType.AMETHYST)
-                    .lightLevel((p_60929_)->{return 5;})
+                    .lightLevel((p_60929_) -> {return 5;})
                     .jumpFactor(2))); // lightLevel & jumpFactor not part of the tutorial, if any inexplicable issues pop up, check this first.
     public static final RegistryObject<Block> RAW_SAPPHIRE_BLOCK = registerBlock("raw_sapphire_block",
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK)
                     .sound(SoundType.AMETHYST)));
 
+    public static final RegistryObject<DropExperienceBlock> SAPPHIRE_ORE = registerBlock("sapphire_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.STONE)
+                    .strength(2f).requiresCorrectToolForDrops(), UniformInt.of(3, 6)));
+    public static final RegistryObject<DropExperienceBlock> DEEPSLATE_SAPPHIRE_ORE = registerBlock("deepslate_sapphire_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE)
+                    .strength(3f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)));
+    public static final RegistryObject<DropExperienceBlock> NETHER_SAPPHIRE_ORE = registerBlock("nether_sapphire_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.NETHERRACK)
+                    .strength(1f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)));
+    public static final RegistryObject<DropExperienceBlock> END_STONE_SAPPHIRE_ORE = registerBlock("end_stone_sapphire_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.END_STONE)
+                    .strength(5f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)));
+
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
-        registerBlockItem(name,toReturn);
+        registerBlockItem(name, toReturn);
         return toReturn;
     }
 
